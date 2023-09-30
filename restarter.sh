f7199920089bd5b66b6b7dc8485198bc1a09ddfd
viktorkovarik <viktorkovarik@gmail.com>
2023-09-30 13:09:00 +0200

    cleaning up un-needed deployments from restarter.sh

--- restarter.sh
+++ restarter.sh
@@ -1,29 +1,14 @@
 cargo build --target wasm32-unknown-unknown --release --package relay
 wasm-opt target/wasm32-unknown-unknown/release/relay.wasm --strip-debug -Oz -o target/wasm32-unknown-unknown/release/relay-opt.wasm
 
-
-dfx canister create internet_identity --specified-id be2us-64aaa-aaaaa-qaabq-cai
-dfx deploy internet_identity --argument '(null)'
-
-
-dfx canister create vetkd_system_api --specified-id br5f7-7uaaa-aaaaa-qaaca-cai
-dfx deploy vetkd_system_api
-
-
 dfx canister create backend --specified-id asrmz-lmaaa-aaaaa-qaaeq-cai
 dfx deploy backend
-
 
 dfx canister create frontend --specified-id bw4dl-smaaa-aaaaa-qaacq-cai
 dfx deploy frontend
 
-
 #dfx canister create relay --specified-id bd3sg-teaaa-aaaaa-qaaba-cai
 #dfx deploy relay
-
-
-#dfx canister create foreign_relay --specified-id b77ix-eeaaa-aaaaa-qaada-cai
-#dfx deploy foreign_relay
 
 dfx canister create ckbtc_ledger --specified-id mxzaz-hqaaa-aaaar-qaada-cai
 #dfx deploy ckbtc_ledger
