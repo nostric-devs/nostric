7d76078ac9c735cb25fdbc9c4f913ccd9767bd50
Lukáš Vožda <vozdalukas@gmail.com>
2023-11-14 21:22:20 +0100

    added II, ledger, vetkeys canisters

--- src/frontend/src/routes/+page.svelte
+++ src/frontend/src/routes/+page.svelte
@@ -1,31 +1,35 @@
 <script lang="ts">
-	import { createActor } from '../../../declarations/backend';
 	import "../../../app.postcss";
-	let input = '';
-	let disabled = false;
-	let greeting = '';
+	import {
+    createActor,
+	} from "../../../declarations/backend";
+	import { AuthClient } from "@dfinity/auth-client";
+	import { HttpAgent } from "@dfinity/agent";
+	import * as vetkd from "../../vetkd_user_lib/ic_vetkd_utils.js";
 
-	const handleOnSubmit = async () => {
-		disabled = true;
 
-		try {
-			// Canister IDs are automatically expanded to .env config - see vite.config.ts
-			const canisterId = import.meta.env.VITE_BACKEND_CANISTER_ID;
+	const logIn = async () => {
+		let authClient = await AuthClient.create();
+    
+    // start the login process and wait for it to finish
+    await new Promise((resolve) => {
+        authClient.login({
+            identityProvider:
+                process.env.DFX_NETWORK === "ic"
+                    ? "https://identity.ic0.app" :
+                    `http://${process.env.INTERNET_IDENTITY_CANISTER_ID}.localhost:8000/#authorize`,
+            onSuccess: resolve,
+        });
+    });
+    const identity = authClient.getIdentity();
+    const agent = new HttpAgent({ identity });
+    let actor = createActor(import.meta.env.VITE_INTERNET_IDENTITY_CANISTER_ID, {
+        agent,
+    });
+		console.log(authClient.isAuthenticated())
+    return false;
+};
 
-			// We pass the host instead of using a proxy to support NodeJS >= v17 (ViteJS issue: https://github.com/vitejs/vite/issues/4794)
-			const host = import.meta.env.VITE_HOST;
-
-			// Create an actor to interact with the IC for a particular canister ID
-			const actor = createActor(canisterId, { agentOptions: { host } });
-
-			// Call the IC
-			greeting = await actor.greet(input);
-		} catch (err: unknown) {
-			console.error(err);
-		}
-
-		disabled = false;
-	};
 </script>
 
 <main>
@@ -33,15 +37,8 @@
 	<br />
 	<br />
 
-	<form on:submit|preventDefault={handleOnSubmit}>
-		<label for="name">Hello, Enter your name: &nbsp;</label>
-		<input class="input" id="name" alt="Name" type="text" bind:value={input} {disabled}  />
-		<button type="submit" class="btn variant-filled">Button</button>
-	</form>
+	<button type="button" on:click={logIn} class="btn variant-filled">Login</button>
 
-	<section id="greeting">
-		{greeting}
-	</section>
 </main>
 
 <style lang="scss">
@@ -50,33 +47,5 @@
 		max-height: 25vw;
 		display: block;
 		margin: auto;
-	}
-
-	form {
-		display: flex;
-		justify-content: center;
-		gap: 0.5em;
-		flex-flow: row wrap;
-		max-width: 40vw;
-		margin: auto;
-		align-items: baseline;
-		font-family: sans-serif;
-		font-size: 1.5rem;
-	}
-
-	button[type='submit'] {
-		padding: 5px 20px;
-		margin: 10px auto;
-		float: right;
-	}
-
-	#greeting {
-		margin: 10px auto;
-		padding: 10px 60px;
-		border: 1px solid #222;
-	}
-
-	#greeting:empty {
-		display: none;
 	}
 </style>
