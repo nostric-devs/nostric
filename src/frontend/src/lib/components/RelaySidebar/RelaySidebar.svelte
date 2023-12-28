4a4330ee06ad1b8a6806cdebea44975f94b91fd4
Lukáš Vožda <lukasvozda@Lukass-MacBook-Pro.local>
2023-12-28 21:28:44 +0100

    adding server icon

--- src/frontend/src/lib/components/RelaySidebar/RelaySidebar.svelte
+++ src/frontend/src/lib/components/RelaySidebar/RelaySidebar.svelte
@@ -1,9 +1,14 @@
+<script>
+    import { Server } from "svelte-feathers";
+</script>
+
 <h3 class="h3 my-6">Relays</h3>
 <ul class="list mb-4">
   <!-- Load relays from store once ready -->
   {#each [1, 2, 3, 5] as relay, index}
     <li class="p-4 variant-glass-tertiary">
       <!-- <span><Server /></span> -->
+      <span><Server /></span>
       <span class="flex-auto">wss://relay.com</span>
       <span class="badge variant-filled-success">Connected</span>
     </li>
