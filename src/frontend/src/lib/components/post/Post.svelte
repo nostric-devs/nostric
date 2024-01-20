4cc701d33cff5dab94354d0868e97f649c7b61e7
Lukáš Vožda <lukasvozda@Lukass-MacBook-Pro.local>
2024-01-20 19:15:43 +0100

    Comments kick off

--- src/frontend/src/lib/components/post/Post.svelte
+++ src/frontend/src/lib/components/post/Post.svelte
@@ -5,6 +5,7 @@
   import { events } from "$lib/stores/Events";
   import { nostrHandler, Reactions } from "$lib/nostr";
   import Avatar from "$lib/components/user/Avatar.svelte";
+  import { Avatar as AvatarSkeleton } from "@skeletonlabs/skeleton";
   import { onMount } from "svelte";
   import dayjs from "dayjs";
   import { AuthStates, authUser } from "$lib/stores/Auth";
@@ -98,6 +99,35 @@
           <Bookmark size="20" class="cursor-pointer" />
         </div>
       {/if}
+    </div>
+    <!-- This represents how a reply to a post should be displayed in the feed -->
+    <!-- This should not be visible for every post by default, only when somebody I follow gives a comment to a post recently (like on Twitter) so we can see the original post they give reaction to -->
+    <div class="grid grid-cols-[auto_1fr] gap-2 ml-8 mr-5 mb-8">
+      <!-- Link to the author -->
+      <a href="#">
+        <AvatarSkeleton src="https://i.pravatar.cc/?img=48" width="w-12" />
+      </a>
+      <div class="card p-4 variant-soft rounded-tl-none space-y-2">
+        <header class="flex justify-between items-center">
+          <!-- Link to the author -->
+          <a href="#">
+            <p class="font-bold">John Doe</p>
+          </a>
+          <small class="opacity-50">2023-02-1 14:00</small>
+        </header>
+        <!-- Link to the post to see comments down the rabbit hole -->
+        <a href="#">
+          <p>Hey, I am replying to your post. What you write is super cool.</p>
+        </a>
+        {#if $authUser.authState !== AuthStates.ANONYMOUS}
+          <div class="flex justify-between pt-2">
+            <Heart size="20" class="cursor-pointer" />
+            <MessageCircle size="20" class="cursor-pointer" />
+            <Share size="20" class="cursor-pointer" />
+            <Bookmark size="20" class="cursor-pointer" />
+          </div>
+        {/if}
+      </div>
     </div>
   {/if}
 {/await}
--- src/frontend/src/routes/(app)/post/[id]/+page.svelte
+++ src/frontend/src/routes/(app)/post/[id]/+page.svelte
@@ -4,9 +4,14 @@
   import type { NDKEvent } from "@nostr-dev-kit/ndk";
   import { nostrHandler } from "$lib/nostr";
   import { page } from "$app/stores";
+  import { Avatar } from "@skeletonlabs/skeleton";
+  import { AuthStates, authUser } from "$lib/stores/Auth";
+  import { Bookmark, Heart, MessageCircle, Share } from "svelte-feathers";
 
   let eventPromise: Promise<NDKEvent>;
   let repliesPromise: Promise<NDKEvent[]>;
+
+  let reply: string = "";
 
   onMount(async () => {
     eventPromise = nostrHandler.fetchEventById($page.params.id);
@@ -30,3 +35,90 @@
     {/each}
   {/if}
 {/await}
+<div>
+  <form class="modal-form m-5">
+    <label class="label">
+      <textarea
+        class="textarea px-3 py-2"
+        rows="3"
+        bind:value={reply}
+        placeholder="Start typing your reply"
+      />
+    </label>
+    <div class="flex justify-end">
+      <button class="btn variant-filled mt-2">Post reply</button>
+    </div>
+  </form>
+</div>
+<div class="m-5">
+  <!-- Only display comment to the post/event displayed, to see comments on the comment user needs to click on the comment to go down the rabbit hole -->
+  <div class="grid grid-cols-[auto_1fr] gap-2 my-4">
+    <!-- Link to the author -->
+    <a href="#">
+      <Avatar src="https://i.pravatar.cc/?img=48" width="w-12" />
+    </a>
+    <div class="card p-4 variant-soft rounded-tl-none space-y-2">
+      <header class="flex justify-between items-center">
+        <!-- Link to the author -->
+        <a href="#">
+          <p class="font-bold">John Doe</p>
+        </a>
+        <small class="opacity-50">2023-02-1 14:00</small>
+      </header>
+      <!-- Link to the post to see comments down the rabbit hole -->
+      <a href="#">
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi
+          commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id
+          pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id
+          velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed
+          quis velit. Nulla facilisi. Nulla libero. Vivamus pharetra posuere
+          sapien.
+        </p>
+      </a>
+      {#if $authUser.authState !== AuthStates.ANONYMOUS}
+        <div class="flex justify-between pt-2">
+          <Heart size="20" class="cursor-pointer" />
+          <MessageCircle size="20" class="cursor-pointer" />
+          <Share size="20" class="cursor-pointer" />
+          <Bookmark size="20" class="cursor-pointer" />
+        </div>
+      {/if}
+    </div>
+  </div>
+  <!-- Second comment -->
+  <div class="grid grid-cols-[auto_1fr] gap-2 my-4">
+    <!-- Link to the author -->
+    <a href="#">
+      <Avatar src="https://i.pravatar.cc/?img=48" width="w-12" />
+    </a>
+    <div class="card p-4 variant-soft rounded-tl-none space-y-2">
+      <header class="flex justify-between items-center">
+        <!-- Link to the author -->
+        <a href="#">
+          <p class="font-bold">John Doe</p>
+        </a>
+        <small class="opacity-50">2023-02-1 14:00</small>
+      </header>
+      <!-- Link to the post to see comments down the rabbit hole -->
+      <a href="#">
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi
+          commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id
+          pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id
+          velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed
+          quis velit. Nulla facilisi. Nulla libero. Vivamus pharetra posuere
+          sapien.
+        </p>
+      </a>
+      {#if $authUser.authState !== AuthStates.ANONYMOUS}
+        <div class="flex justify-between pt-2">
+          <Heart size="20" class="cursor-pointer" />
+          <MessageCircle size="20" class="cursor-pointer" />
+          <Share size="20" class="cursor-pointer" />
+          <Bookmark size="20" class="cursor-pointer" />
+        </div>
+      {/if}
+    </div>
+  </div>
+</div>
