4c867c3c60c36f72d464b339ec1d4f825a6618c8
Lukáš Vožda <lukasvozda@Lukass-MacBook-Pro.local>
2024-01-20 14:36:49 +0100

    Custom css switched to tailwind css

--- src/frontend/src/lib/components/images/ImageTile.svelte
+++ src/frontend/src/lib/components/images/ImageTile.svelte
@@ -36,23 +36,25 @@
 </script>
 
 <div
-  class="image-container relative w-full overflow-hidden rounded-md before:block before:pt-[100%]"
+  class="image-container relative w-full overflow-hidden rounded-md before:block before:pt-[100%] hover:opacity-100 transition-opacity duration-300"
 >
   <img
-    class="absolute inset-0 w-full h-full object-cover rounded-md"
+    class="absolute inset-0 w-full h-full object-cover rounded-md transition-all duration-300"
     src={url}
     alt=""
   />
-  <div class={`overlay rounded-md ${deleting ? "deleting" : ""}`}></div>
   <div
-    class={`image-icons absolute inset-0 flex items-center justify-center ${
-      deleting ? "deleting" : ""
+    class={`overlay absolute inset-0 bg-white bg-opacity-80 transition-opacity duration-300 pointer-events-none ${deleting ? "opacity-100" : "opacity-0"}`}
+  ></div>
+  <div
+    class={`image-icons absolute inset-0 flex flex-col items-center justify-center gap-2.5 transition-opacity duration-300 opacity-0 pointer-events-none ${
+      deleting ? "opacity-100 pointer-events-auto" : ""
     }`}
   >
     <button
       use:clipboard={url}
       type="button"
-      class="btn variant-filled-primary font-normal"
+      class="btn variant-filled-primary font-normal transition duration-300"
       disabled={deleting}
       on:click={() => {
         copied = true;
@@ -62,7 +64,7 @@
       <span>
         <Copy size="16" class="lg:mx-auto xl:mx-0" />
       </span>
-      <span class="text-sm transition-all">
+      <span class="text-sm">
         {copied ? "Copied 👍" : "Copy URL"}
       </span>
     </button>
@@ -70,7 +72,7 @@
     <button
       on:click={() => deleteFile(url)}
       type="button"
-      class="btn variant-filled-warning font-normal"
+      class="btn variant-filled-warning font-normal transition duration-300"
       disabled={deleting}
     >
       {#if deleting}
@@ -86,49 +88,3 @@
     </button>
   </div>
 </div>
-
-<style>
-  .image-container {
-    position: relative;
-    overflow: hidden;
-  }
-  .image-container img {
-    transition: all 0.3s ease;
-    display: block;
-  }
-  .overlay {
-    display: block;
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: rgba(255, 255, 255, 0.8);
-    opacity: 0;
-    transition: opacity 0.3s ease;
-    pointer-events: none; /* Prevents overlay from blocking mouse events */
-  }
-  .image-container:hover .overlay {
-    opacity: 1;
-  }
-  .image-icons {
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    transition: opacity 0.3s ease;
-    opacity: 0;
-    pointer-events: none; /* Prevents icons from blocking mouse events */
-  }
-  .image-container:hover .image-icons,
-  .deleting {
-    opacity: 1;
-    pointer-events: auto; /* Allows interaction with the icons */
-  }
-  .btn {
-    transition: background-color 0.3s ease;
-  }
-</style>
