0855baa7a2d6a3a57df9af15b2370742c22c6268
Lukáš Vožda <lukasvozda@Lukass-MacBook-Pro.local>
2024-02-10 18:40:16 +0100

    Profile form working

--- src/frontend/src/routes/(app)/settings/profile/+page.svelte
+++ src/frontend/src/routes/(app)/settings/profile/+page.svelte
@@ -2,11 +2,9 @@
   import { getToastStore, clipboard } from "@skeletonlabs/skeleton";
   import type { ToastStore } from "@skeletonlabs/skeleton";
   import { authUser } from "$lib/stores/Auth";
-  import { feed, type NodeEvent } from "$lib/stores/Feed";
-  import type { NDKEvent, NDKUserProfile } from "@nostr-dev-kit/ndk";
-  import { Copy } from "svelte-feathers";
+  import type { NDKUserProfile } from "@nostr-dev-kit/ndk";
+  import { Circle, Copy } from "svelte-feathers";
 
-  let events: NodeEvent[] = [];
   const toastStore: ToastStore = getToastStore();
   let disabled: boolean = false;
   let loading: boolean = false;
@@ -26,56 +24,78 @@
     });
   };
 
-  $: {
-    let parsedEvents: NodeEvent[] = [];
-    for (const event of Object.values($feed) as NodeEvent[]) {
-      if (
-        event.isRoot() &&
-        event.model.author.pubkey === $authUser.nostr?.getPublicKey()
-      ) {
-        parsedEvents.push(event.model);
+  const updateOnSubmit = async () => {
+    loading = true;
+    disabled = true;
+    try {
+      if ($authUser.nostr) {
+        console.log(userProfile);
+        await $authUser.nostr.updateProfile(userProfile);
+        toastStore.trigger({
+          message: `Your profile ${userProfile.name} was sucessfully updated.`,
+          background: "variant-filled-success",
+          classes: "rounded-2xl, font-semibold",
+        });
       }
-      parsedEvents = parsedEvents.sort(
-        (x: NDKEvent, y: NDKEvent) => y.created_at - x.created_at,
-      );
+    } catch (error) {
+      toastStore.trigger({
+        message: error as string,
+        background: "variant-filled-error",
+        classes: "rounded-2xl, font-semibold",
+      });
+    } finally {
+      loading = false;
+      disabled = false;
     }
-    events = parsedEvents;
-  }
+  };
+
   $: userProfile = $authUser.nostr.getUser()?.profile;
 </script>
 
-<!-- <UserProfile {user} {events} /> -->
 <h1 class="h1 m-4">Profile Settings</h1>
-<form method="POST" class="w-full">
+<form class="w-full p-4">
   <div class="mt-6">
-    <div class="font-semibold pl-1 mb-2">Your new username</div>
+    <div class="font-semibold pl-1 mb-2">Username</div>
     <input
       type="text"
       name="userName"
-      class="input px-3 py-2 rounded-2xl mr-2 mb-4"
+      class="input px-3 py-2 rounded-md mr-2 mb-4"
       placeholder="type in your new Nostr user name"
       bind:value={userProfile.name}
       {disabled}
     />
   </div>
+  <div>
+    <div class="font-semibold pl-1 mb-2">Profile picture URL</div>
+    <div>
+      <input
+        type="text"
+        name="userName"
+        class="input px-3 py-2 rounded-md mr-2 mb-4"
+        placeholder="Type your image URL"
+        bind:value={userProfile.image}
+        {disabled}
+      />
+    </div>
+  </div>
   <div class="mb-8">
-    <div class="font-semibold pl-1 mb-2">Your bio</div>
+    <div class="font-semibold pl-1 mb-2">Bio</div>
     <div>
       <textarea
-        class="textarea rounded-2xl"
-        rows="3"
+        class="textarea rounded-md"
+        rows="6"
         name="bio"
         bind:value={userProfile.about}
       ></textarea>
     </div>
   </div>
   <div class="mb-4">
-    <div class="font-semibold pl-1 mb-2">Your new private key</div>
+    <div class="font-semibold pl-1 mb-2">Private key</div>
     <div class="flex">
       <input
         type="text"
         name="privateKey"
-        class="input px-3 py-2 rounded-2xl mr-3"
+        class="input px-3 py-2 rounded-md mr-3"
         data-clipboard="privateKey"
         value={$authUser.nostr?.getPrivateKey()}
         readonly
@@ -83,7 +103,7 @@
       <button
         type="button"
         use:clipboard={{ input: "privateKey" }}
-        class="btn variant-filled rounded-2xl font-semibold"
+        class="btn variant-filled rounded-md font-semibold"
         on:click={() =>
           triggerClipboardToast("Private key copied to clipboard")}
       >
@@ -93,12 +113,12 @@
     </div>
   </div>
   <div class="mb-4">
-    <div class="font-semibold pl-1 mb-2">Your new public key</div>
+    <div class="font-semibold pl-1 mb-2">Public key</div>
     <div class="flex">
       <input
         type="text"
         name="publicKey"
-        class="input px-3 py-2 rounded-2xl mr-3"
+        class="input px-3 py-2 rounded-md mr-3"
         data-clipboard="publicKey"
         value={$authUser.nostr?.getPublicKey()}
         readonly
@@ -106,7 +126,7 @@
       <button
         type="button"
         use:clipboard={{ input: "publicKey" }}
-        class="btn variant-filled rounded-2xl font-semibold"
+        class="btn variant-filled rounded-md font-semibold"
         on:click={() => triggerClipboardToast("Public key copied to clipboard")}
       >
         <span>Copy</span>
@@ -115,9 +135,8 @@
     </div>
   </div>
   <button
-    type="submit"
-    class="btn bg-gradient-to-r from-pink-600 to-violet-500 rounded-2xl w-full font-semibold mb-6"
-    formaction="/sign-in?/login-identity"
+    on:click={updateOnSubmit}
+    class="btn variant-filled-primary mt-8 rounded-md w-full font-semibold mb-6"
     disabled={disabled || !userProfile.name}
   >
     {#if loading}
@@ -125,14 +144,6 @@
         <Circle size="15" color="white" unit="px"></Circle>
       </span>
     {/if}
-    Create new account with Internet Identity
-    <span class="ml-2">
-      <!-- <img
-      src={DfinityLogo}
-      alt="dfinity-logo"
-      width="25px"
-      height="10px"
-    /> -->
-    </span>
+    Update your profile
   </button>
 </form>
