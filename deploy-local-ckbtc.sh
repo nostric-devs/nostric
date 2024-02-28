ecdf64e3a592283402df8d899b1183eee8d3dbc1
viktorkovarik <viktorkovarik@gmail.com>
2024-02-28 09:38:40 +0100

    Fix creating backup files. (#102)

--- deploy-local-ckbtc.sh
+++ deploy-local-ckbtc.sh
@@ -6,9 +6,12 @@ if [ "$DFX_NETWORK" != "ic" ]; then
       SED_INPLACE="-i"
   else
       # macOS (BSD sed)
-      SED_INPLACE="-i ''"
+      SED_INPLACE="-i .bak"
   fi
   sed $SED_INPLACE 's/service : {/service : (ledger_arg : LedgerArg) -> {/' icrc1.local.did
   dfx deploy ckbtc_ledger
   sed $SED_INPLACE 's/service : (ledger_arg : LedgerArg) -> {/service : {/g' icrc1.local.did
+  if [ -f "icrc1.local.did.bak" ]; then
+      rm "icrc1.local.did.bak"
+  fi
 fi
